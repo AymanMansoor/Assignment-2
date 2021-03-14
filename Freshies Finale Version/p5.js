e7f852a6a43eb78b6dc28759557feb9788d987a5
flo351 <79473110+flo351@users.noreply.github.com>
2021-03-14 17:59:01 -0400

    Add files via upload

---
+++ Freshies Finale Version/p5.js
@@ -0,0 +1,41 @@
+function validate()
+{
+    document.getElementById('myForm')
+	var emailt = document.getElementById("email");
+	var password = document.getElementById("password");
+    var reg = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+  
+	if(emailt.value == "")
+	{
+		alert("Please enter your email.");
+		return false;
+	}
+    
+	if(password.value == "")
+	{
+		alert("Please enter your password.");
+		return false;
+	}
+    
+    if (!reg.test(emailt.value) )  
+    {
+      alert("Please provide a valid email address");
+      email.focus;
+      return false;
+    }
+    else{(emailt.value == reg && password.value == pwd)
+        alert("Login successfully!"); 
+        
+        window.location="index.html";
+      
+    }
+	
+}
+
+
+  
+
+
+
+
---
+++ Freshies Finale Version/p6.js
@@ -0,0 +1,50 @@
+function validate()
+{
+    document.getElementById('myForm')
+    
+    var firstname = document.getElementById("firstname");
+    var lastname = document.getElementById("lastname");
+	var email_add = document.getElementById("email_add");
+	var password = document.getElementById("password");
+    var passwordconfirmation = document.getElementById("passwordconfirmation");
+    var postalcode = document.getElementById("postalcode");
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    var regExp =/[A-Z\d[A-Z]\s\d[A-Z]\d/;
+
+	
+	if(email_add.value == "") 
+	{
+		alert("Please enter your email.");
+		return false;
+	} else if(reg.test(email_add.value) == false) 
+    {
+            alert('Invalid Email Address');
+            return false;
+    }
+	if(password.value != passwordconfirmation.value) 
+	{
+		alert("The two passwords are not the same.\n"  + "Please re-enter.");
+		return false;
+	}else if(password.value == "" || passwordconfirmation.value == "") {
+	    alert("Please enter your password.");
+		return false;
+	}
+    if(regExp.test(postalcode.value) == false) {
+        alert("Invalid Zip Code");
+        return false;
+    }
+  if( firstname.value != "" && lastname.value != "") 
+    {
+        alert("Registration successful!"); 
+        window.location="index.html";
+        return false;
+    }else{
+        alert("Please complete first name or last name");
+    }
+    
+
+    
+}
+
+
+  
