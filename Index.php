b950f04b8f84b346d8a8b7131fcff00da8021bc8
j-5844 <78096076+j-5844@users.noreply.github.com>
2021-04-17 20:33:42 -0400

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,21 @@
+<?php
+include('header.php');
+?>
+
+<div class="container banner">
+    <h4 class="banner-title">Freshies</h4>
+    <h2>Always Buy Fresh!</h2>
+    <p class="banner-para">Here at Freshies, we always strive to provide the freshest ingredients for our customers and the best online services possible.</p>
+    <a href="Aisles.html" class="btn-primary">Shop Now &#10132</a>
+</div>
+
+
+<div class="container banner-2">
+    <p class="banner-para2">Get 25% off your first order when you Sign Up!</p>
+    <a href="Signup.html" class="btn-sec">Sign Up Now &#10132</a>
+</div>
+
+
+<?php
+include('footer.php');
+?>
