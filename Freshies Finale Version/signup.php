e1df2fc694930904a058bf10dbaf23e0f1203470
flo351 <79473110+flo351@users.noreply.github.com>
2021-04-18 02:12:19 -0400

    Add files via upload

--- Freshies Finale Version/signup.php
+++ Freshies Finale Version/signup.php
@@ -9,15 +9,15 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-<script type="text/javascript" src="signup.php"></script>
+
 <head>
     <meta charset="UTF-8">
-    <title>Freshies - Sign Up</title>
+    <title>Freshies - Login</title>
     <link rel="stylesheet" href="p5p6.css">
-   
+    <script src="p6.js"></script>
 </head>
 
-<body class="aisles">
+<body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="Index.html">Freshies</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,7 +29,7 @@
                     <a class="nav-link" href="Index.html">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">All Products</a>
+                    <a class="nav-link" href="ProductList.html">All Products</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="Aisles.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Aisles</a>
@@ -49,56 +49,82 @@
                     <a class="nav-link" href="ShopCart.html"><img src="images/Cart.png" width="18px" height="18px"> Cart</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="login.html"><img src="images/Login.png" width="18px" height="18px"> Login</a>
+                    <a class="nav-link" href="Signup.html"><img src="images/User.png" width="18px" height="18px"> Sign Up</a>
                 </li>
         </div>
     </nav>
-    <div>
-        <br>
-        <div class="container col-sm-6">
-            <h2>Sign up</h2>
-            <br>
-            <div class="text col-sm-7">
-                <form action= "signup.php" method="POST" onsubmit=" return validate()">
-                    <div>
-                        <label for="firstname">First name:</label><br>
-                        <input type="text col-sm-9" name="firstname" id="firstname" size="30" /><br/> </br>
-
-                        <label for="lastname">Last name:</label><br>
-                        <input type="text" name="lastname" id="lastname" size="30" /><br> <br>
-
-                        <label for="postalcode">Postal code (ex: H7T 3B7) : </label><br>
-                        <input type="text" name="postalcode" id="postalcode" size="30"><br> <br>
-
-                        <label for="email">Email: </label><br>
-                        <input type="text" id="email_add" name="email" size="30" /><br> <br/>
-
-                        <label for="password">Password: </label><br>
-                        <input type="password" name="pass" id="password" size="30"><br> <br/>
-
-                        <label for="passwordconfirmation">Password confirmation: </label><br>
-                        <input type="password" name="passwordconfirmation" id="passwordconfirmation" size="30" /><br> <br>
-                        <input type="submit"  name="submit" role="button" value="submit" class="btn btn-info" >
-                        <button type="reset" name="reset" class="btn btn-info" id="reset">Reset</button>
-                        <p>Already have an account ? <a href="login.html" style="color:red">Log in</a>.</p>
-                        <script type="text/javascript" src="p6.js"></script> 
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-
-    <footer class="footer bg-light text-center">
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-            <p>
-                © 2021 Copyright: <a class="text-dark" href="Index.html">Freshies.html</a><br/> Have questions about your order? Please <a class="text-dark" href="ContactUs.html">Contact Us</a><br/>
-            </p>
-        </div>
-    </footer>
+    <?php
+     
+      $firstname = $_POST["firstname"];
+      $lastname = $_POST["lastname"];
+      $postalcode = $_POST["postalcode"];
+      $email = $_POST["email"];
+      $password = $_POST["password"]; ?>
+    <br>
+    <div class="container col-md-12">
+        <div class="text ">
+            <div>
+    <h2>Sign up information </h2>
+    
+    <?php
+    
+      print ("First name :  $firstname  <br />");
+      print ("Last name :  $lastname  <br />"); 
+      print ("Postal code :  $postalcode  <br />");
+      print ("Email :  $email  <br />");
+     
+   
+      
+    ?>
+    <?php
+    $out = fopen("register_user","a");
+    fputs($out,"$_POST[firstname]\n");
+    fputs($out,"$_POST[lastname]\n");
+    fputs($out,"$_POST[postalcode]\n");
+    fputs($out,"$_POST[email]\n");
+    fputs($out,"$_POST[password]\n\n");
+    
 
 
+    ?>
+    <?php
+if(isset($_POST["email"]) && isset($_POST["pass"]))
+{
+    
+    $file=fopen("data.txt","r");
+    $finduser = false;
+    while(!feof($file))
+    {
+        $line = fgets($file);
+        $array = explode(";",$line);
+        if(trim($array[0]) == $_POST['email'])
+        {
+            $finduser=true;
+            break;
+        }
+    }
+    fclose($file);
+ 
+    
+    if( $finduser )
+    {
+        echo $_POST["email"];
+        echo "<script>alert('user already exists please Log in ');</script>";
+        echo'<script> window.location="Login.html"; </script> ';
+        
+    }
+    else
+    {
+        $file = fopen("data.txt", "a");
+        fputs($file,$_POST["email"].";".$_POST["pass"]."\r\n");
+        fclose($file);
+        echo $_POST["email"];
+        echo " registered successfully!";
+    }
+}
+else
+{
+    include 'signup.html';
+}
+?>
 
-
-</body>
-
-</html>
