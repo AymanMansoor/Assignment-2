35bd436a9272e02497cde7b633003c462582b222
j-5844 <78096076+j-5844@users.noreply.github.com>
2021-04-17 20:35:17 -0400

    Add files via upload

---
+++ Aisles.php
@@ -0,0 +1,31 @@
+<?php
+include('header.php');
+?>
+    
+    <h3>Aisles</h3>
+    <div class="container">
+        <div class="row">
+          <div class="col col-sm, deal"><a class="aisle-style" href="WeeklyDeals.html">WEEKLY DEALS</a></div>
+      
+          <div class="w-100"></div>
+          <div class="col col-sm, img-aisle-1"><a class="aisle-style" href="DairyEggs.html">DAIRY & EGGS</a></div>
+          <div class="col col-sm, img-aisle-2"><a class="aisle-style" href="FruitVegie.html">FRUITS & VEGETABLES</a></div>
+      
+          <div class="w-100"></div>
+      
+          <div class="col col-sm, img-aisle-3"><a class="aisle-style" href="Snacks.html">SNACKS</a></div>
+          <div class="col col-sm, img-aisle-4"><a class="aisle-style" href="Meat.html">MEAT & POULTRY</a></div>
+          
+
+          <div class="w-100"></div>
+
+          <div class="col col-sm, img-aisle-5"><a class="aisle-style" href="Bread.html">BREAD & BAKERY</a></div>
+          <div class="col col-sm, img-aisle-6"><a class="aisle-style" href="#">BEVERAGES</a></div>
+          
+
+        </div>
+      </div>
+
+<?php
+include('footer.php');
+?>
